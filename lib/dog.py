b5be5423e2031966e4c2a4ee0e467602c3420366
collins <kipkorirc583@gmail.com>
2023-08-22 11:43:38 +0300

    First commit

--- lib/dog.py
+++ lib/dog.py
@@ -12,4 +12,13 @@ APPROVED_BREEDS = [
 ]
 
 class Dog:
-    pass
+    def __init__(self,name="",breed="pug") :
+        if not(type(name)in (str,)) and (1<=len(name)<=25):
+            print(f"Name must be string between 1 and 25 characters.")
+        elif breed not in APPROVED_BREEDS:
+            print("Breed must be in list of approved breeds.") 
+        else:
+            self.name=name
+            self.breed=breed
+               
+    
--- lib/person.py
+++ lib/person.py
@@ -15,5 +15,14 @@ APPROVED_JOBS = [
     "Purchasing"
 ]
 
+
 class Person:
-    pass
+    def __init__(self,name="",job="Sales") :
+        if not(type(name)in (str,)) and (1<=len(name)<=25):
+            print(f"Name must be string between 1 and 25 characters.")
+        elif job not in APPROVED_JOBS:
+            print("Job must be in list of approved jobs.") 
+        else:
+            self.name=name
+            self.job=job
+               
